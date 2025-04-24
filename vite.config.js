ab55c173bb0a6c855b67f5f14a4588be5b2d6ad3
andrelefranco001 <andrelefranco001@gmail.com>
2025-04-24 18:28:02 -0500

    fix: Update base URL in Vite configuration and ensure gh-pages dependency is included

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://andrelefranco001.github.io/workanap/",
+  base: "https://andrelefranco001.github.io/interactive_ad/",
 });
